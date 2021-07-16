7163140a5f03eb67da01d513634f8779742e2075
Simeon Trenev <65025840+SimeonTrenev@users.noreply.github.com>
2021-07-16 14:14:47 +0300

    comment

--- authService.js
+++ authService.js
@@ -1,30 +1,28 @@
-const mongoose = require('mongoose')
-const User = mongoose.model('User');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const { SECRET } = require('./config');
+const { SECRET } = require("./config");
 
 const register = (username, password) => {
-    let user = new User({ username, password });
+  let user = new User({ username, password });
 
-    return user.save();
+  return user.save();
 };
 
-
 const login = async (email, password) => {
-    let user = await User.findOne({ email });
-    if (!user) throw new Error('User does not exist!')
-    let areEqual = await bcrypt.compare(password, user.password)
-    if (!areEqual) throw new Error('Passwords does not match!')
+  let user = await User.findOne({ email });
+  if (!user) throw new Error("User does not exist!");
+  let areEqual = await bcrypt.compare(password, user.password);
+  if (!areEqual) throw new Error("Passwords does not match!");
 
-    let token = jwt.sign({ _id: user._id, email: user.email }, SECRET);
+  let token = jwt.sign({ _id: user._id, email: user.email }, SECRET);
 
-    return token;
-
+  return token;
 };
 
 module.exports = {
-    register,
-    login,
-}
+  register,
+  login,
+};
