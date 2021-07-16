71743b35a10c96e747bc45bdd03e9e9172e49584
Simeon Trenev <65025840+SimeonTrenev@users.noreply.github.com>
2021-07-16 13:48:44 +0300

    commit

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const mongoPath =
   "mongodb+srv://admin:12345@myfirstcluster.zbh03.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const bodyParser = require("body-parser");
 const cors = require("cors");
-// require("./models/Offers");
-// require("./models/ConstructionTypes");
-// require("./models/User");
+require("./models/Offers");
+require("./models/ConstructionTypes");
+require("./models/User");
 
 const cookieParser = require("cookie-parser");
 // const auth = require("./auth");
@@ -16,8 +16,8 @@ let app = express();
 const port = process.env.PORT || 9000;
 app.use(cors());
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 // app.use(auth);
 
@@ -35,15 +35,15 @@ app.use((req, res, next) => {
   next();
 });
 
-// require("./routes/test")(app);
+require("./routes/test")(app);
 
-// app.post("/register", (req, res, next) => {
-//   console.log("asd");
-//   res.send("testa");
-// });
+app.post("/register", (req, res, next) => {
+  console.log("asd");
+  res.send("testa");
+});
 app.post("/add-offer", (req, res, next) => {
   console.log(req.body);
-  // res.send(req.body);
+  res.send(req.body);
   res.send(req.body);
 });
 
